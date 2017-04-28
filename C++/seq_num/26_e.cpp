0e971f80ae71f20329117178d1ee351722892951
Changsong <changsong.zhu@one.verizon.com>
2017-04-27 22:24:17 -0700

    Add solution for 26 and 27

---
+++ C++/seq_num/26_e.cpp
@@ -0,0 +1,26 @@
+/**
+26[E]. Remove Duplicates from the Sorted Array
+ 
+Given a sorted array, remove the duplicates in place such that each element appear only once and return the new length.
+Do not allocate extra space for another array, you must do this in place with constant memory.
+For example,
+Given input array nums = [1,1,2],
+Your function should return length = 2, with the first two elements of nums 
+
+
+**/
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if(nums.size()==0) return 0;
+
+        int index=0;
+        for(int i=1;i<nums.size();i++)
+        {
+            if(nums[i] != nums[index])
+                nums[++index]=nums[i];
+        }
+        return index+1;
+    }
+};

decc2b66a722da91a47595f61f86702c55c5a611
April Yuan <april@Aprils-iMac.local>
2017-08-18 00:22:51 -0700

    Refine Solution 21

--- C++/seq_num/021_e.cpp
+++ C++/seq_num/021_e.cpp
@@ -56,16 +56,17 @@ public:
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        ListNoe dummy(-1);
+        ListNode dummy(-1);
         ListNode *prev=&dummy;
         while(l1||l2)
         {
-            prev->next=(l1&&l2)?(l1->val<l2->va?l1:l2):(l1?l1:l2);
+            ListNode *tmp=((l1&&l2)?(l1->val>l2->val?l2:l1):(l1?l1:l2));
+            prev->next=tmp;
             prev=prev->next;
-            l1?l1->next:l1;
-            l2?l2->next:l2;
+            l1=(tmp==l1?l1->next:l1);
+            l2=(tmp==l2?l2->next:l2);           
         }
         return dummy.next;
+        
     }
 };
-

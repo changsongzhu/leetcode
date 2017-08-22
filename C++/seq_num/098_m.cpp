47260e2ab1f674a15261af8123e452b75887b939
April Yuan <april@Aprils-iMac.local>
2017-08-21 21:59:01 -0700

    Refine solution 098

--- C++/seq_num/098_m.cpp
+++ C++/seq_num/098_m.cpp
@@ -1,32 +1,4 @@
-/**
-96[M]. Unique BST
- 
-Given n, how many structurally unique BST's (binary search trees) that store values 1...n?
-For example,
-Given n = 3, there are a total of 5 unique BST's.
-   1     	3 	3      2  	1
-    \   	/ 	/      / \  	\
-     3 	2 	1      1   3  	2
-    / 	/   	\             	\
-   2 	1     	2             	3
-class Solution {
-public: 
-    int numTrees(int n) {
-        if(n<=0) return 0;
-        vector<int> dp(n+1, 0);
-        dp[0]=dp[1]=1;
-        for(int i=2;i<=n;i++)
-        {
-            for(int j=0;j<i;j++)
-            {
-               dp[i]+=dp[j]*dp[i-1-j];
-            }
-        }
-        return dp[n];
-    }
-};  
- 
- 
+/** 
 98[M]. Valid Binary Search Tree
  
 Given a binary tree, determine if it is a valid binary search tree (BST).

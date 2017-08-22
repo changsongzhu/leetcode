1aeeb3d92ba25b386b9709cd1b875986e4302595
Changsong <changsong.zhu@one.verizon.com>
2017-08-22 16:58:23 -0700

    Refine Solution 38

--- C++/seq_num/038_e.cpp
+++ C++/seq_num/038_e.cpp
@@ -1,6 +1,5 @@
 /**
 38[E]. Count and Say
- 
 The count-and-say sequence is the sequence of integers beginning as follows:
 1, 11, 21, 1211, 111221, ...
 1 is read off as "one 1" or 11.
@@ -9,7 +8,6 @@ The count-and-say sequence is the sequence of integers beginning as follows:
 Given an integer n, generate the nth sequence.
 Note: The sequence of integers will be represented as a string.
 **/
-
 class Solution {
 public:
     string countAndSay(int n) {
@@ -37,9 +35,38 @@ public:
                {
                    count++;
                }
-                
             }
             res=tmp;
+        }
+        return res;
+    }
+};
+
+//Refined Version
+class Solution {
+public:
+    string countAndSay(int n) {
+        if(n<=0) return "";
+        string res="1";
+        for(int i=1;i<n;i++)
+        {
+           string tmp="";
+           res.push_back('*');
+           int count=0;
+           for(int j=0;j<res.size();j++)
+           {
+               if(j!=0&&res[j]!=res[j-1])
+               {
+                   tmp.push_back(count+'0');
+                   tmp.push_back(res[j-1]);
+                   count=1;
+               }
+               else
+               {
+                   count++;
+               }
+           }
+           res=tmp;
         }
         return res;
     }

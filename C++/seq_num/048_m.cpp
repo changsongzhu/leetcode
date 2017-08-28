21ddbd75a8a682a88eda0d4a83884106e3458930
Changsong <changsong.zhu@one.verizon.com>
2017-08-28 16:55:39 -0700

    Refine Solution 48

--- C++/seq_num/048_m.cpp
+++ C++/seq_num/048_m.cpp
@@ -6,6 +6,31 @@ Rotate the image by 90 degrees (clockwise).
 Follow up:
 Could you do this in-place?
 **/
+
+/*
+
+clockwise rotate
+
+1 2
+3 4
+
+--> up<-->down  3 4
+                1 2
+
+--> diagnosal   3 1
+                4 2
+
+anti-clockwise rotate
+
+1 2
+3 4
+--> up<-->down 3 4
+               1 2
+
+--> anti-diagnosal  2 4
+                    1 3
+
+*/
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
@@ -25,6 +50,30 @@ public:
            {
                swap(matrix[i][j], matrix[j][i]);
            }
+        }
+    }
+};
+
+
+//Anti-clockwise
+class Solution {
+public:
+    void rotate_2(vector<vector<int> >&m)
+    {
+        if(m.size()<2||m.size()!=m[0].size()) return;
+        int r=m.size(), c=m.size();
+        int up=0,down=r-1;
+        while(up<down)
+        {
+            for(int i=0;i<c;i++
+                swap(m[up][i], m[down][i]);
+            up++;
+            down--;
+        }
+        for(int i=0;i<r;i++)
+        {
+            for(int j=r-i, k=1;j<c;j++, k++)
+                swap(m[i][j], m[i-k][j-k]);
         }
     }
 };

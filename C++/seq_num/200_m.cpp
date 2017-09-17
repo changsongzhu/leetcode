42ba8486131fbae57da56cede2f15ae327745029
Changsong <changsong.zhu@one.verizon.com>
2017-09-16 22:33:21 -0700

    Add BFS solution for 200

--- C++/seq_num/200_m.cpp
+++ C++/seq_num/200_m.cpp
@@ -14,6 +14,47 @@ Example 2:
 Answer: 3
 **/
 
+//BFS Solution
+class Solution {
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        if(grid.size()==0||grid[0].size()==0) return 0;
+        queue<pair<int, int> > q;
+        int res=0;
+        int m=grid.size();
+        int n=grid[0].size();
+        vector<vector<bool> > visited(m, vector<bool>(n, false));
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(grid[i][j]=='1'&&visited[i][j]==false)
+                {
+                    res++;
+                    q.push({i, j});
+                    vector<vector<int> >  dirs ={{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+                    while(!q.empty())
+                    {
+                        int row=q.front().first,col=q.front().second;q.pop();
+                        for(int k=0;k<dirs.size();k++)
+                        {
+                            int x=row+dirs[k][0], y=col+dirs[k][1];
+                            if(x<0||x>=m||y<0||y>=n||grid[x][y]!='1'||visited[x][y]==true) continue;
+                            visited[x][y]=true;
+                            q.push({x, y});
+                        }
+                    }
+                }
+            }
+        }
+        return res;
+        
+    }
+};
+
+
+
+//DFS Soltuion
 class Solution {
 public:
     int numIslands(vector<vector<char>>& grid) {

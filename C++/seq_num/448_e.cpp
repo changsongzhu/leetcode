4406fcf9aa7bd881ab4c09c8d18099192ad02756
Changsong <changsong.zhu@one.verizon.com>
2017-09-29 16:34:36 -0700

    Refine solution 448

--- C++/seq_num/448_e.cpp
+++ C++/seq_num/448_e.cpp
@@ -13,6 +13,22 @@ Output:
 
 **/
 
+//Follow up solution
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+        vector<int> res;
+        for(int i=0;i<nums.size();i++)
+        {
+            while(nums[i]!=i+1&&nums[i]!=nums[nums[i]-1])
+                swap(nums[i], nums[nums[i]-1]);
+        }
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]!=i+1) res.push_back(i+1);
+        }
+        return res;
+    }
+};
 
 class Solution {
 public:

97df028f81da30062f14f8b0dd9ea7770c4c6ecb
Changsong <changsong.zhu@one.verizon.com>
2017-09-29 15:50:08 -0700

    Refine Solution 442

--- C++/seq_num/442_m.cpp
+++ C++/seq_num/442_m.cpp
@@ -5,6 +5,24 @@ Find all the elements that appear twice in this array.
 Could you do it without extra space and in O(n) runtime?
 **/
 
+//follow up Solution
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        vector<int> res;
+        for(int i=0;i<nums.size();i++)
+        {
+            while(nums[i]!=i+1&&nums[i]!=nums[nums[i]-1])
+                swap(nums[i], nums[nums[i]-1]);
+        }
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]!=i+1) res.push_back(nums[i]);
+        }
+        return res;
+    }
+};
+
 class Solution {
 public:
     vector<int> findDuplicates(vector<int>& nums) {

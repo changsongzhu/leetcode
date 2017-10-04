83ecad6ed701af6bc34877b369957e8a2b482469
Changsong <changsong.zhu@one.verizon.com>
2017-10-04 13:00:22 -0700

    Add new solution for 506

--- C++/seq_num/506_e.cpp
+++ C++/seq_num/506_e.cpp
@@ -11,6 +11,31 @@ Note:
 N is a positive integer and won't exceed 10,000.
 All the scores of athletes are guaranteed to be unique.
 **/
+//Set Solution
+class Solution {
+public:
+    vector<string> findRelativeRanks(vector<int>& nums) {
+        set<pair<int, int> > s;
+        int n=nums.size();
+        vector<string> res(n, "");
+        for(int i=0;i<nums.size();i++)
+        {
+            s.insert({nums[i], i});
+        }
+        int i=0;
+        for(auto a:s)
+        {
+           if(i==n-1)res[a.second]="Gold Medal";
+           else if(i==n-2)res[a.second]="Silver Medal";
+           else if(i==n-3)res[a.second]="Bronze Medal";
+           else
+               res[a.second]=to_string(n-i);
+           i++;
+        }
+        return res;
+    }
+};
+
 class Solution {
 public:
     vector<string> findRelativeRanks(vector<int>& nums) {

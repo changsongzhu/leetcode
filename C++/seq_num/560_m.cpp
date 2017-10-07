cee3709162771ec1c94a342ca9e08d2a94554743
Changsong <changsong.zhu@one.verizon.com>
2017-10-06 19:48:26 -0700

    Add new solution for 560

--- C++/seq_num/560_m.cpp
+++ C++/seq_num/560_m.cpp
@@ -9,6 +9,23 @@ Note:
 The length of the array is in range [1, 20,000].
 The range of numbers in the array is [-1000, 1000] and the range of the integer k is [-1e7, 1e7].
 **/
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        map<int, int> m;
+        int sum=0;
+        int cnt=0;
+        m[0]++;
+        for(int i=0;i<nums.size();i++)
+        {
+            sum+=nums[i];
+            cnt+=m[sum-k];
+            m[sum]++;
+        }
+        return cnt;
+    }
+};
+
 
 class Solution {
 public:

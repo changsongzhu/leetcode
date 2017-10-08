5d0a0fe18d0eeb996a2e69612d53c0bfb8b19706
April Yuan <april@Aprils-iMac.local>
2017-10-07 17:51:53 -0700

    Refine Solution 41

--- C++/seq_num/041_h.cpp
+++ C++/seq_num/041_h.cpp
@@ -7,6 +7,24 @@ Your algorithm should run in O(n) time and uses constant space.
 **/
 
 
+//Refined Solution
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        int n=nums.size();
+        for(int i=0;i<n;i++)
+        {
+            while(nums[i]>0&&nums[i]<=n&&nums[i]!=i+1&&nums[i]!=nums[nums[i]-1])
+                swap(nums[i], nums[nums[i]-1]);
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]!=i+1) return i+1;
+        }
+        return n+1;
+    }
+};
+
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {

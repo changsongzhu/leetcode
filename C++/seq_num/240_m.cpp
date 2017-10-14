fe411bac3beb54293143a380f8199bfc4f5e56b9
April Yuan <april@Aprils-iMac.local>
2017-10-13 23:59:53 -0700

    Refine Solution 240

--- C++/seq_num/240_m.cpp
+++ C++/seq_num/240_m.cpp
@@ -15,6 +15,28 @@ Consider the following matrix:
 Given target = 5, return true.
 Given target = 20, return false.
 **/
+
+//Refined Solution
+class Solution {
+public:
+bool searchMatrix(vector<vector<int>>& matrix, int target) {
+    int m = matrix.size();
+    if (m == 0) return false;
+    int n = matrix[0].size();
+
+    int i = 0, j = n - 1;
+    while (i < m && j >= 0) {
+        if (matrix[i][j] == target)
+            return true;
+        else if (matrix[i][j] > target) {
+            j--;
+        } else 
+            i++;
+    }
+    return false;
+  }
+};
+
  
 class Solution {
 public:

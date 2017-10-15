c5aa2d2ba4fd528e179799e91183ff50e8769135
Changsong <changsong.zhu@one.verizon.com>
2017-10-15 15:23:11 -0700

    Refine solution 311

--- C++/seq_num/311_m.cpp
+++ C++/seq_num/311_m.cpp
@@ -19,6 +19,32 @@ B = [
 AB = | -1 0 3 | x | 0 0 0 | = | -7 0 3 |
  	             | 0 0 1 |
 **/
+
+//Refined Solution
+class Solution {
+public:
+    vector<vector<int>> multiply(vector<vector<int>>& A, vector<vector<int>>& B) {
+        vector<vector<int> >  res(A.size(), vector<int>(B[0].size(), 0));
+        for(int i=0;i<A.size();i++)
+        {
+            for(int k=0;k<A[i].size();k++)
+            {
+                if(A[i][k])
+                {
+                    for(int j=0;j<B[0].size();j++)
+                    {
+                        if(B[k][j])
+                        {
+                            res[i][j]+=A[i][k]*B[k][j];
+                        }
+                    }
+                }
+            }
+        }
+        return res;
+    }
+};
+
  
  class Solution {
 public:

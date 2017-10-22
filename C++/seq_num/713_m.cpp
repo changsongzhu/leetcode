94158f8f6d630556817c8585552c4e3c1eb17285
Changsong <changsong.zhu@one.verizon.com>
2017-10-21 20:50:57 -0700

    Add Solution 712 713 714

---
+++ C++/seq_num/713_m.cpp
@@ -0,0 +1,38 @@
+/**
+713[M]. Subarray Product Less than K
+Your are given an array of positive integers nums.
+
+Count and print the number of (contiguous) subarrays where the product of all the elements in the subarray is less than k.
+
+Example 1:
+Input: nums = [10, 5, 2, 6], k = 100
+Output: 8
+Explanation: The 8 subarrays that have product less than 100 are: [10], [5], [2], [6], [10, 5], [5, 2], [2, 6], [5, 2, 6].
+Note that [10, 5, 2] is not included as the product of 100 is not strictly less than k.
+Note:
+
+0 < nums.length <= 50000.
+0 < nums[i] < 1000.
+0 <= k < 10^6.
+**/
+
+
+class Solution {
+public:
+    int numSubarrayProductLessThanK(vector<int>& nums, int k) {
+        int left=0;
+        int res=0;
+        int prod=1;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]<k) res++;
+            while(left<i&&prod*nums[i]>=k)
+            {
+                prod/=nums[left++];
+            }
+            prod*=nums[i];
+            res+=i-left;
+        }
+        return res;
+    }
+};

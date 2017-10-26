b96f9fa9722e1464d70abd77d76d43b3c8351cad
Changsong <changsong.zhu@one.verizon.com>
2017-10-26 14:42:15 -0700

    Add DP solution for 221

--- C++/seq_num/221_m.cpp
+++ C++/seq_num/221_m.cpp
@@ -9,6 +9,29 @@ For example, given the following matrix:
 1 0 0 1 0
 Return 4.
 **/
+
+
+//DP Solution
+class Solution {
+public:
+    int maximalSquare(vector<vector<char>>&matrix){
+        if(matrix.size()==0||matrix[0].size()==0) return 0;
+        int m=matrix.size(), n=matrix[0].size();
+	int mx=0;
+        vector<vector<int> > dp(m, vector<int>(n, 0));
+        for(int i=0;i<m;i++) { dp[i][0]=(matrix[i][0]=='1'?1:0); mx=max(mx, dp[i][0]);}
+        for(int i=0;i<n;i++) { dp[0][i]=(matrix[0][i]=='1'?1:0); mx=max(mx, dp[0][i]);}
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                dp[i][j]=matrix[i][j]=='1'?min(dp[i-1][j-1], min(dp[i][j-1], dp[i-1][j]))+1:0;
+                mx=max(mx, dp[i][j]);
+            }
+        }
+        return mx*mx;
+    }
+};
+
+
 class Solution {
 public:
     int maximalSquare(vector<vector<char>>& matrix) {

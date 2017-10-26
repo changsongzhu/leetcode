3993c7d92728f28531a1bc44a8685006b599913f
April Yuan <april@Aprils-iMac.local>
2017-10-25 22:04:54 -0700

    Add Stack and DP solution for 032

--- C++/seq_num/032_h.cpp
+++ C++/seq_num/032_h.cpp
@@ -4,6 +4,53 @@ Given a string containing just the characters '(' and ')', find the length of th
 For "(()", the longest valid parentheses substring is "()", which has length = 2.
 Another example is ")()())", where the longest valid parentheses substring is "()()", which has length = 4.
 **/
+
+
+//DP Solution
+class Solution{
+public:
+    int longestValidParentheses(string s){
+        int n=s.size();
+        vector<int> dp(n, 0);
+        int res=0;
+        for(int i=1;i<s.size();i++)
+        {
+            if(s[i]==')')
+            {
+                if(s[i-1]=='(') dp[i]=dp[i-2]+2;
+                if(s[i-1]==')'&&s[i-dp[i-1]-1]=='(') dp[i]=dp[i-1]+dp[i-dp[i-1]-2]+2;
+            }
+            res=max(res, dp[i]);
+        }
+        return res;
+    }
+};
+
+
+//Stack Solution
+class Solution{
+public:
+    int longestValidParentheses(string s){
+        stack<int> stk;
+        int res=0;
+        stk.push(-1);
+        for(int i=0;i<s.size();i++)
+        {
+            if(s[i]==')'&&s[stk.top()]=='(')
+            {
+                stk.pop();
+                res=max(res, i-stk.top());
+            }
+            else
+            {
+                stk.push(i);
+            }
+        }
+        return res;
+    }
+};
+
+//Brute Force Solution
 class Solution {
 public:
     int longestValidParentheses(string s) {

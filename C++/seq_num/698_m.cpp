c2eb59c78b1b1e417cdfa494aebb29a2f72e9259
Changsong <changsong.zhu@one.verizon.com>
2017-10-26 17:33:03 -0700

    Refine the solution 698

--- C++/seq_num/698_m.cpp
+++ C++/seq_num/698_m.cpp
@@ -11,6 +11,38 @@ Note:
 1 <= k <= len(nums) <= 16.
 0 < nums[i] < 10000.
 **/
+//Refined Solution
+class Solution {
+public:
+    bool canPartitionKSubsets(vector<int>& nums, int k) {
+        if(nums.size()<k) return false;
+        int sum=accumulate(nums.begin(), nums.end(), 0);
+        if((sum%k)!=0) return false;
+        int avg=sum/k;
+        vector<int> sums(k, 0);
+        sort(nums.rbegin(), nums.rend());
+        return dfs(nums, sums, 0, avg, k);  
+    }
+    
+    bool dfs(vector<int>& nums, vector<int> &sums, int pos, int target, int k)
+    {
+        if(pos==nums.size())
+        {
+            return true;
+        }
+
+        for(int i=0;i<k;i++)
+        {
+           if(sums[i]+nums[pos]>target) continue;
+           sums[i]+=nums[pos];
+           if(dfs(nums, sums, pos+1, target, k)) return true;
+           sums[i]-=nums[pos];
+           if(sums[i]==0) break;
+        }
+        return false;
+    }
+};
+
 
 class Solution {
 public:

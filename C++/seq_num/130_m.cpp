386a7672f44600ec58df93884569807dfb2380b2
Changsong <changsong.zhu@one.verizon.com>
2017-10-27 13:27:26 -0700

    Refine Solution 130

--- C++/seq_num/130_m.cpp
+++ C++/seq_num/130_m.cpp
@@ -14,6 +14,39 @@ X X X X
 X O X X
 **/
 
+//Refined Solution
+class Solution{
+public:
+    void solve(vector<vector<char>>  &board){
+        for(int i=0;i<board.size();i++)
+        {
+            for(int j=0;j<board[0].size();j++)
+            {
+                if((i==0||i==board.size()-1||j==0||j==board[0].size()-1)&&board[i][j]=='O')
+                    dfs(board, i, j);
+            }
+        }
+
+        for(int i=0;i<board.size();i++){
+            for(int j=0;j<board[0].size();j++){
+                if(board[i][j]=='O') board[i][j]='X';
+                if(board[i][j]=='*') board[i][j]='O';
+            }
+        }
+
+    }
+    void dfs(vector<vector<char> >&board, int x, int y)
+    {
+        if(x<0||y<0||x>=board.size()||y>=board[0].size()||board[x][y]!='O') return;
+        board[x][y]='*';
+        dfs(board, x-1, y);
+        dfs(board, x+1, y);
+        dfs(board, x, y-1);
+        dfs(board, x, y+1);
+    }
+};
+
+
 class Solution {
 public:
     void solve(vector<vector<char>>& board) {

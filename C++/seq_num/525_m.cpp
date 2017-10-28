b4066ce59cc88ac7ed3b3489e7327cd52398a7bb
Changsong <changsong.zhu@one.verizon.com>
2017-10-28 09:31:34 -0700

    Refine solution 525

--- C++/seq_num/525_m.cpp
+++ C++/seq_num/525_m.cpp
@@ -11,6 +11,29 @@ Output: 2
 Explanation: [0, 1] (or [1, 0]) is a longest contiguous subarray with equal number of 0 and 1.
 Note: The length of the given binary array will not exceed 50,000.
 **/
+class Solution{
+public:
+    int findMaxLength(vector<int> &nums){
+        if(nums.size()==0) return 0;
+        map<int, int> m;
+        m[0]=-1;
+        int res=0, sum=0;
+        for(int i=0;i<nums.size();i++){
+            sum+=(nums[i]==0)?-1:1;
+            //never update the first sum's index because we want to get the maximum length
+            if(m.count(sum))
+            {
+                res=max(res, i-m[sum]);
+            }
+            else
+            {
+                m[sum]=i;
+            }
+        }
+        return res;
+    }
+};
+
 
 class Solution {
 public:

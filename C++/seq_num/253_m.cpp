27e03921b3a4167713ba60ff3619f9a8815fe5da
Changsong <changsong.zhu@one.verizon.com>
2017-11-01 09:05:33 -0700

    Add Minheap solution for Meeting Room II

--- C++/seq_num/253_m.cpp
+++ C++/seq_num/253_m.cpp
@@ -7,6 +7,25 @@ Given [[0, 30],[5, 10],[15, 20]],
 return 2.
 **/
 
+//MinHeap Solution
+class Solution {
+public:
+    int minMeetingRooms(vector<Interval> &intervals){
+        if(intervals.size()==0) return 0;
+        sort(intervals.begin(), intervals.end(), [](Interval &a, Interval &b){return a.start<b.start;});
+        priority_queue<int, vector<int>, greater<int>> pq;
+        for(auto a:intervals)
+        {
+            if(!pq.empty()&&a.start>=pq.top()) pq.pop();
+            pq.push(a.end);
+        }
+        return pq.size();
+
+    }
+};
+
+
+//Wrong Solution
 class Solution {
 public:
     int minMeetingRooms(vector<Interval>& intervals) {
@@ -19,7 +38,7 @@ public:
            if(intervals[i].start<right)
            {
               res++;
-              right=min(right, interval[i].end;
+              right=min(right, intervals[i].end);
            }
        }
        return res;

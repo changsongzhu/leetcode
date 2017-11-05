2039147951d4bb21b4e622d59c55ffb3116e053b
April Yuan <april@Aprils-iMac.local>
2017-11-05 15:22:49 -0800

    Add O(N2) Solution for 259

--- C++/seq_num/259_m.cpp
+++ C++/seq_num/259_m.cpp
@@ -10,6 +10,32 @@ Follow up:
 Could you solve it in O(n2) runtime?
 **/
 
+
+//O(N2) Solution
+class Solution {
+public:
+    int threeSumSmaller(vector<int>& nums, int target) {
+        if(nums.size()<3) return 0;
+        sort(nums.begin(), nums.end());
+        int res=0;
+        for(int i=0;i<nums.size()-2;i++)
+        {
+            if(nums[i]+nums[i+1]+nums[i+2]>=target) break;
+            int j=i+1, k=nums.size()-1;
+            while(j<k)
+            {
+                while(j<k&&nums[i]+nums[j]+nums[k]>=target)k--;
+                res+=k-j;
+                j++;
+            }
+        }
+        return res;
+        
+    }
+};
+
+
+//O(N3) Solution
 class Solution {
 public:
     int threeSumSmaller(vector<int>& nums, int target) {

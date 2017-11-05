56772aa40da58ef89b345ccaad7d7c27c6645068
Changsong <changsong.zhu@one.verizon.com>
2017-11-04 20:00:19 -0700

    Add new solution for 395

--- C++/seq_num/395_m.cpp
+++ C++/seq_num/395_m.cpp
@@ -18,6 +18,30 @@ Output:
  
 The longest substring is "ababb", as 'a' is repeated 2 times and 'b' is repeated 3 times.
 **/
+
+class Solution {
+public:
+    int longestSubstring(string s, int k){
+        if(s.size()==0||k>s.size()) return 0;
+        if(k==0) return s.size();
+        map<char, int>m;
+        for(auto c:s) m[c]++;
+        int idx=-1;
+        for(int i=0;i<s.size();i++)
+        {
+            if(m[s[i]]<k)
+            {
+                idx=i;
+                break;
+            }
+        }
+        if(idx==-1) return s.size();
+        int left=longestSubstring(s.substr(0, idx), k);
+        int right=longestSubstring(s.substr(idx+1), k);
+        return max(left, right);
+    }
+};
+
  
 class Solution {
 public:

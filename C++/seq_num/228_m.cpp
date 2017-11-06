46bc38f62a645d3e3f7dfc5ead2f4e66924a90f9
April Yuan <april@Aprils-iMac.local>
2017-11-05 16:00:46 -0800

    Refine Solution 228

--- C++/seq_num/228_m.cpp
+++ C++/seq_num/228_m.cpp
@@ -4,6 +4,30 @@ Given a sorted integer array without duplicates, return the summary of its range
 For example, given [0,1,2,4,5,7], return ["0->2","4->5","7"].
 **/
 
+//Refined Solution
+class Solution {
+public:
+    vector<string> summaryRanges(vector<int>& nums) {
+        vector<string> res;
+        for(int i=0;i<nums.size();i++)
+        {
+            int j=i;
+            while(j+1<nums.size()&&nums[j+1]==nums[j]+1)j++;
+            if(j!=i)
+            {
+                res.push_back(to_string(nums[i])+"->"+to_string(nums[j]));
+                i=j;
+            }
+            else
+            {
+                res.push_back(to_string(nums[i]));
+            }
+        }
+        return res;
+    }
+};
+
+
 class Solution {
 public:
     vector<string> summaryRanges(vector<int>& nums) {

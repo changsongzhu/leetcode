f178221093d3b421d1e7ad9ccfa176cb1f3e3733
Changsong <changsong.zhu@one.verizon.com>
2017-11-07 15:00:15 -0800

    Add O(1) solution for 234

--- C++/seq_num/234_e.cpp
+++ C++/seq_num/234_e.cpp
@@ -36,6 +36,51 @@ public:
 };
 
 
+class Solution {
+public:
+    bool isPalindrome(ListNode *head){
+        if(head==NULL||head->next==NULL) return true;
+
+        ListNode *mid=findMiddle(head);
+        ListNode *right=reverseList(mid->next);
+        mid->next=NULL;
+        ListNode *p=head;
+        ListNode *q=right;
+        while(p&&q)
+        {
+            if(p->val!=q->val) return false;
+            p=p->next;
+            q=q->next;
+        }
+        ListNode *node=reverseList(right);
+        mid->next=node;
+        return true;
+    }
+    ListNode *findMiddle(ListNode *head){
+        ListNode *fast=head->next;//let the fast move one more step
+        ListNode *slow=head;
+        while(fast&&fast->next)
+        {
+            fast=fast->next->next;
+            slow=slow->next;
+        }
+        return slow;
+    }
+    ListNode *reverseList(ListNode *head){
+        ListNode *prev=NULL;
+        ListNode *cur=head;
+        while(cur)
+        {
+            ListNode *tmp=cur->next;
+            cur->next=prev;
+            prev=cur;
+            cur=tmp;
+        }
+        return prev;
+    }
+};
+
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {

e7082b5bf0e39db8bba102f09a286489d847914d
Changsong <changsong.zhu@one.verizon.com>
2017-11-09 11:21:21 -0800

    Add new solution for 008

--- C++/seq_num/008_m.cpp
+++ C++/seq_num/008_m.cpp
@@ -9,6 +9,29 @@ Notes: It is intended for this problem to be specified vaguely (ie, no given inp
 class Solution {
 public:
     int myAtoi(string str) {
+        if(str.size()==0) return 0;
+        int i=0;
+        while(i<str.size()&&str[i]==' ')i++;
+        int sign=1;
+        if(i<str.size()&&(str[i]=='+'||str[i]=='-')) sign=(str[i++]=='+'?1:-1);
+        int base=0;
+        for(;i<str.size();i++)
+        {
+            if(str[i]>'9'||str[i]<'0') break;
+            if(base>INT_MAX/10||(base==INT_MAX/10&&str[i]>'7'))
+            {
+                return sign==1?INT_MAX:INT_MIN;
+            }
+            base=base*10+str[i]-'0';
+        }
+        return base*sign;
+    }
+};
+
+
+class Solution {
+public:
+    int myAtoi(string str) {
        if(str.size()==0) return 0;
        int base=0, sign=1, i=0,n=str.size();
        while(i<n&&str[i]==' ') i++;

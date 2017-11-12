b521e46338f9af9d1dfb15996f01b066e51e49ec
April Yuan <april@Aprils-iMac.local>
2017-11-11 21:24:30 -0800

    Add new solution for 073

--- C++/seq_num/073_m.cpp
+++ C++/seq_num/073_m.cpp
@@ -3,6 +3,41 @@
 Given a m x n matrix, if an element is 0, set its entire row and column to 0. Do it in place.
 **/
 
+
+//Refined Solution
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        if(matrix.size()==0||matrix[0].size()==0) return;
+        set<int> row;
+        set<int> col;
+        int m=matrix.size(), n=matrix[0].size();
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(matrix[i][j]==0)
+                {
+                    row.insert(i);
+                    col.insert(j);
+                }
+            }
+        }
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(row.count(i)||col.count(j))
+                    matrix[i][j]=0;
+            }
+        }
+        
+    }
+};
+
+
+
+
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {

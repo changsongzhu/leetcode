f6b8524f291bae8efafb962bdb576ae7e124d9f4
Changsong <changsong.zhu@one.verizon.com>
2017-11-22 19:17:31 -0800

    Refine the Parking lot

--- C++/OOD/parking_lot.cpp
+++ C++/OOD/parking_lot.cpp
@@ -1,7 +1,9 @@
+/*
 Parking Lot Design
+*/
 
-enum class ParkingType{
-    REGULAR =1,
+enum ParkingType{
+    REGULAR,
     HANDICAPPED,
     COMPACT,
     MAX_PARKING_TYPE,
@@ -31,123 +33,53 @@ public:
         this->parkingSpace=parkingSpace;
         for(int i=0;i<parkingSpace.size();i++)
         {
-             if(parkingSpace[i].ParkingType==REGULAR)
-                 vacantRegularParking.push({parkingSpace[i].distance, i});
-            else if(parkingSpace[i].ParkingType==HANDICAPPED)
-                 vacantCompactParking.push({parkingSpace[i].distance, i});
-            else
-                 vacantHandicappedParking.push({parkingSpace[i].distance, i});
+            freeParkingSpace({parkingSpace[i].distance, i});
         }
+        this->freeParkingSpace=parkingSpace.size();
+        this->totalParkingSpace=parkingSpace.size();
     }
 
-    bool parkVehicle( Vehicle &vehicle)
-    {
-       bool parked=false;
-       int parkIdx=-1;
-       if(vehicle.ParkingType==HANDICAPPED&&vacantHandicappedParking.size()!=0)
-        {
-            auto v=vacantHandicappedParking.top();vacantHandicappedParking.pop();
-            parkIdx=v.second;
-            occupiedHandicappedParking.push_back({parkingSpace[v.second].distance, v.second});
-            parked=true;
+    bool parkVehicle( Vehicle &vehicle){
+        vector<pair<int, int> > tmp;
+        while(!freeParkingSpace.empty()){
+            auto a=freeParkingSpace.top();freeParkingSpace.pop();
+            if(a.parkingType==vehicle.parkingType){
+                parkingSpace[a.second].vehicle=vehicle;
+                parkingSpace[a.second].isVacant=false;
+                m[vehicle.plateId]=a.second;
+                break;
+            }else {
+                tmp.push_back(a);
+            }
         }
-        if(parked==false&&vacantRegularParking.size()!=0)
-        {
-auto v=vacantRegularParking.top();vacantRegularParking.pop();
-            parkIdx=v.second;
-            occupiedRegularParking.push_back({parkingSpace[v.second].distance, v.second});
-            parked=true;
-        }
-        if(parked==false&&vacantCompactParking.size()!=0)
-        {
-auto v=vacantCompactParking.top();vacantCompactParking.pop();
-            parkIdx=v.second;
-            occupiedCompactParking.push_back({parkingSpace[v.second].distance, v.second});
-            parked=true;
-        }
-        if(parked)
-        {
-            parkingSpace[parkIdx].isVacant=false;
-            parkingSpace[parkIdx].vehicle=vehicle;
-            freeParkingSpace--;
-        }
-        return parked;
+        for(int i=0;i<tmp.size();i++)freeParkingSpace.push(tmp[i]);
+        return m.count(vehicle.plateId)!=0;
     }
    
 
- void releaseVehicle(Vehicle &vehicle)
+    void releaseVehicle(Vehicle &vehicle)
     {
-         int i=0;
-         for(;i<parkingSpace.size();i++)
-         {
-              if(parkingSpace[i].vehicle==vehicle)
-                  break;
-         }
-         if(parkingSpace[i].parkingType==HANDICAPPED)
-         {
-            pair<int, int> last=occupiedHandicappedParking[occupiedHandicappedParking.size()];
-            for(int i=0;i<occupiedHandicappedParking.size();i++)
-            {
-                if(occupiedHandicappedParking[i].second==i)
-                {
-                    occupiedHandicappedParking[i]=last;
-                    occupiedHandicappedParking.pop_back();
-                }
-            }
-            parkingSpace[i].isVacant=true;
-            vacantHandicappedParking(parkingSpace[i].distance, i);
-         }
-         else if(parkingSpace[i].parkingType==REGULAR)
-         {
-            pair<int, int> last=occupiedRegularParking[occupiedRegularParking.size()];
-            for(int i=0;i<occupiedRegularParking.size();i++)
-            {
-                if(occupiedRegularParking[i].second==i)
-                {
-                    occupiedRegularParking[i]=last;
-                    occupiedRegularParking.pop_back();
-                }
-            }
-            parkingSpace[i].isVacant=true;
-            vacantRegularParking(parkingSpace[i].distance, i);
-         }
-         else if(parkingSpace[i].parkingType==COMPACT)
-         {
-            pair<int, int> last=occupiedCompactParking[occupiedCompactParking.size()];
-            for(int i=0;i<occupiedCompactParking.size();i++)
-            {
-                if(occupiedCompactParking[i].second==i)
-                {
-                    occupiedCompactParking[i]=last;
-                    occupiedCompactParking.pop_back();
-                }
-            }
-            parkingSpace[i].isVacant=true;
-            vacantCompactParking(parkingSpace[i].distance, i);
-         }
-         freeParkingSpace++;
+        if(m.count(vehicle.plateId)==0) return;
+        int idx=m[vehicle.plateId];
+        parkingSpace[idx].isVacant=false;
+        m.erase(vehicle.plateId);
+        freeParkingSpace.push({parkingSpace[idx].distance, idx});
     }   
 
     int getTotalParkingSpace()
     {
-         return totalParkingSpace;
+         return parkingSpace.size();
     }
 
     int isFull()
     {
-         return freeParkingSpace==0;
+         return !freeParkingSpace.empty();
     }
     
 
 private:
     vector<ParkingSpace> parkingSpace;
-    priority_queue<pair<int, int>, vector<pair<int, int>>, comp> vacantRegularParking;
-    vector<pair<int, int> > occupiedRegularParking;
-    priority_queue<pair<int, int>, vector<pair<int, int>>, comp> vacantCompactParking;
-    vector<pair<int, int> >  occupiedCompactParking;
-    priority_queue<pair<int, int>, vector<pair<int, int>>, comp> vacantHandicappedParking;
-    vector<pair<int, int> >  occupiedHandicappedParking;
-    int totalParkingSpace;
-    int freeParkingSpace;
+    priority_queue<pair<int, int>, vector<pair<int, int> >, comp> freeParkingSpace;
+    unordered_map<string, int> m;
 };
 
